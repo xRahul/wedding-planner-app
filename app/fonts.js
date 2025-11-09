4102fdc2410c4454bb0e60bb0a2eda2c093d7a02
Rahul Jain <xRahul@users.noreply.github.com>
2025-11-09 09:01:02 +0000

    remove all code

--- app/fonts.js
+++
@@ -1,8 +0,0 @@
-import { Inter } from "next/font/google";
-
-export const inter = Inter({
-  weight: ["400", "500", "600"],
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-inter",
-});
